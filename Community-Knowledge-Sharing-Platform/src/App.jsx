6b3490f240c81bd612be52a1a555d0b1c7bae00f
Hady Safa <hadyabdallahsafa@gmail.com>
2025-01-18 12:06:43 +0200

    Added create post form

--- Community-Knowledge-Sharing-Platform/src/App.jsx
+++ Community-Knowledge-Sharing-Platform/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Login from './Forms/Login'
 import SignUp from './Forms/Signup'
 import Homepage from './Homepage'
+import CreatePost from './CreatePost.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<SignUp />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Home" element={<Homepage />} />
+          <Route path="/NewPost" element={<CreatePost />} />
         </Routes>
       </Router>
     </>
---
+++ Community-Knowledge-Sharing-Platform/src/CreatePost.jsx/index.jsx
@@ -0,0 +1,156 @@
+import styles from './style.module.css'
+import { useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function CreatePost() {
+
+    const navigate = useNavigate(null)
+    const form = useRef(null)
+    const title = useRef(null)
+    const description = useRef(null)
+    const category = useRef(null)
+    const link = useRef(null)
+    const code = useRef(null)
+
+    // fill dropdown
+    const [categories, setCategories] = useState([])
+    async function getAllCategories() {
+        const url = "http://localhost/SharingPlatform/api.php/Categories";
+        try {
+            const response = await fetch(url)
+            if (!response.ok) throw new Error("");
+            const data = await response.json()
+            if (data) {
+                setCategories(data)
+            }
+        }
+        catch (error) {
+            setError(error.message)
+        }
+    }
+    useEffect(() => { getAllCategories() }, [])
+
+    // handle submission
+    function handleSubmission(e) {
+        e.preventDefault();
+
+        let submittedTitle = title.current.value;
+        let submittedDescription = description.current.value;
+        let submittedCategory = category.current.value;
+        let submittedLink = link.current.value;
+        let submittedCode = code.current.value;
+
+        const postData = {
+            Title : submittedTitle,
+            Description : submittedDescription,
+            CategoryId : submittedCategory,
+            Link : submittedLink,
+            Code: submittedCode,
+            UserId: 22
+        }
+        
+        if (postData.Title && postData.Description && postData.CategoryId) {
+            addNewPost(postData);
+            form.current.reset();
+            navigate("/Home")
+        }
+    }
+    async function addNewPost(formData) {
+        const url = "http://localhost/SharingPlatform/api.php/Posts";
+        const requestData = formData
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(requestData),
+            });
+            if (!response.ok) throw new Error("Creation Failed");
+            const data = await response.json();
+            if (data) {
+                console.log(data)
+            }
+        } catch (err) {
+            // warning or error may be occured since snippet or link are null, but it doesnt affect the execution of the code 
+            console.log(err.message);
+        }
+    }
+
+
+    return (
+
+        <form ref={form} className={styles.form} method='post' onSubmit={handleSubmission}>
+
+            <h2 className={styles.header}>Create Post </h2>
+
+            <div>
+                <div className={styles.title}>Title <span className={styles.optional}    >(Required)</span></div>
+                <input
+                    type='text'
+                    ref={title}
+                    name='Title'
+                    placeholder='Enter a title for your post'
+                    className={styles.titleInput}
+                />
+            </div>
+
+            <div>
+                <div className={styles.title}>Description <span className={styles.optional}    >(Required)</span></div>
+                <textarea
+                    type='textarea'
+                    ref={description}
+                    name='Description'
+                    placeholder='Enter your post desccription here'
+                    className={styles.descriptionInput}
+                ></textarea>
+            </div>
+
+            <div>
+                <div className={styles.title}>Category <span className={styles.optional}    >(Required)</span></div>
+                <div className={styles.dropdownContainer}>
+                    <select name='Category' ref={category} className={styles.dropdown}>
+                        <option value="">Choose Category</option>
+                        {
+                            categories && categories.length > 0
+                                ? categories.map((object, index) => <option key={index} value={object.Id}>{object.Name}</option>)
+                                : null
+                        }
+                    </select>
+                </div>
+            </div>
+
+            <div>
+                <div className={styles.title}>Link <span className={styles.optional}>(Optional)</span></div>
+                <input
+                    type='text'
+                    ref={link}
+                    name='Link'
+                    placeholder='Attach a link'
+                    className={styles.titleInput}
+                />
+            </div>
+
+            <div>
+                <div className={styles.title}>Code Snippet <span className={styles.optional}    >(Optional)</span></div>
+                <textarea
+                    type='textarea'
+                    ref={code}
+                    name='code'
+                    placeholder='Code snippet'
+                    className={styles.descriptionInput}
+                ></textarea>
+            </div>
+
+            <div className={styles.buttonsContainer}>
+                <button type='reset' className={styles.cancel}>Cancel</button>
+                <button type='submit' className={styles.submit}>Post</button>
+            </div>
+
+        </form>
+
+    )
+
+}
+
+export default CreatePost
